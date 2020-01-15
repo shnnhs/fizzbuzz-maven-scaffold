3083fd50b2490ad1993b0c34c80be646631b5035
桑浩楠 <sanghaonan@sina.com>
2020-01-15 22:58:50 +0800

    代码重新提交

--- src/test/java/NumberGameTest.java
+++ src/test/java/NumberGameTest.java
@@ -1,17 +1,16 @@
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class NumberGameTest {
-    @ParameterizedTest
-    @CsvSource({
-            "1,1",
-            "3,Fizz",
-            "5,Buzz",
-            "15,FizzBuzz"
-    })
-    public void number_game_test(int input,String output) {
-        assertEquals( new NumberGame().show(input), output);
-    }
+   @ParameterizedTest
+   @CsvSource({
+           "1,1",
+           "3,Fizz",
+           "5,Buzz",
+           "15,FizzBuzz"
+   })
+   public void show_test(int input,String output) {
+       assertEquals(new NumberGame().show(input),output);
+   }
 }
